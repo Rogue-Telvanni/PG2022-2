1a079befe73570dc785ad0769b9eea8d28dfe0e7
Rogue-Telvanni <joaocopini11@outlook.com>
2022-08-25 00:15:52 -0300

    correção para não usar mais as funcionalidade da versão compability da openg gl com glad

--- Linux/HelloTriangle/Source.cpp
+++ Linux/HelloTriangle/Source.cpp
@@ -16,7 +16,6 @@ using namespace std;
 
 // GLAD
 #include <glad/glad.h>
-// #include <gl.h>
 
 // GLFW
 #include <GLFW/glfw3.h>
@@ -28,12 +27,10 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 // Prot�tipos das fun��es
 int setupShader();
 int setupGeometry();
-void draw_circle(GLfloat, GLfloat, GLfloat);
+GLfloat* draw_circle(GLfloat, GLfloat, GLfloat, GLfloat, GLint);
 
 // Dimens�es da janela (pode ser alterado em tempo de execu��o)
 const GLuint WIDTH = 800, HEIGHT = 600;
-const float PI = 3.1415926;
-
 // C�digo fonte do Vertex Shader (em GLSL): ainda hardcoded
 const GLchar* vertexShaderSource = "#version 450\n"
 "layout (location = 0) in vec3 position;\n"
@@ -120,7 +117,7 @@ int main()
 		glfwPollEvents();
 
 		// Limpa o buffer de cor
-		glClearColor(0.8f, 0.8f, 0.8f, 1.0f); //cor de fundo
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //cor de fundo
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glLineWidth(10);
@@ -128,18 +125,10 @@ int main()
 
 		// Chamada de desenho - drawcall
 		// Poligono Preenchido - GL_TRIANGLES
-		glUniform4f(colorLoc, 0.0f, 0.0f, 1.0f, 1.0f); //enviando cor para vari�vel uniform inputColor
+		glUniform4f(colorLoc, 0.0f, 0.0f, 1.0f, 1.0f); //enviando cor para variável uniform inputColor
 		glBindVertexArray(VAO);
-   		draw_circle(0, 0, 0.5);
-
-		// glDrawArrays(GL_TRIANGLE_FAN, 0, 3);
-
-		// // Chamada de desenho - drawcall
-		// // CONTORNO - GL_LINE_LOOP
-		// // PONTOS - GL_POINTS
-		// // glUniform4f(colorLoc, 1.0f, 0.0f, 1.0f, 1.0f); //enviando cor para vari�vel uniform inputColor
-		// // glDrawArrays(GL_POINTS, 0, 3);
-		// glBindVertexArray(0);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 9);
+		glBindVertexArray(0);
 
 		// Troca os buffers da tela
 		glfwSwapBuffers(window);
@@ -151,40 +140,52 @@ int main()
 	return 0;
 }
 
-void draw_circle(GLfloat centerX, GLfloat centerY, GLfloat radius)
+GLfloat* draw_circle(GLfloat centerX, GLfloat centerY, GLfloat centerZ, GLfloat radius, GLint segments)
 {
-	int i;
-	int triangleAmount = 20; //# of triangles used to draw circle
+	GLint vertices = segments + 2;
+	GLfloat angle = 2.0f * M_PI;
 
-	// GLfloat radius = 0.8f; //radius
-	
-	GLfloat twicePi = 2.0f * PI;
+	GLfloat verticesX[vertices];
+	GLfloat verticesY[vertices];
+	GLfloat verticesZ[vertices];
 
-	glaglBegin(GL_TRIANGLE_FAN);
-	glVertex2f(centerX, centerY); // center of circle
-	for (i = 0; i <= triangleAmount; i++)
-	{
-		glVertex2f(
-			centerX + (radius * cos(i * twicePi / triangleAmount)),
-			centerY + (radius * sin(i * twicePi / triangleAmount)));
+	// circle center
+	verticesX[0] = centerX;
+	verticesY[0] = centerY;
+	verticesZ[0] = centerZ;
+
+	for(int index = 1; index < vertices; index++){
+		
+		verticesX[index] = centerX + (radius * cos(index * angle / segments));
+		verticesY[index] = centerY + (radius * sin(index * angle / segments));
+		verticesZ[index] = centerZ;
 	}
-	glEnd();
+
+	GLfloat* completeArray = new GLfloat[vertices * 3];
+	
+	for(int i = 0; i < vertices; i++){
+		completeArray[i * 3] = verticesX[i];
+		completeArray[(i * 3) + 1] = verticesY[i];
+		completeArray[(i * 3) + 2] = verticesZ[i];
+	}
+
+	return completeArray;
 }
 
 // Fun��o de callback de teclado - s� pode ter uma inst�ncia (deve ser est�tica se
 // estiver dentro de uma classe) - � chamada sempre que uma tecla for pressionada
 // ou solta via GLFW
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-// Esta fun��o est� basntante hardcoded - objetivo � compilar e "buildar" um programa de
-//  shader simples e �nico neste exemplo de c�digo
-//  O c�digo fonte do vertex e fragment shader est� nos arrays vertexShaderSource e
-//  fragmentShader source no ini�io deste arquivo
-//  A fun��o retorna o identificador do programa de shader
+//Esta fun��o est� basntante hardcoded - objetivo � compilar e "buildar" um programa de
+// shader simples e �nico neste exemplo de c�digo
+// O c�digo fonte do vertex e fragment shader est� nos arrays vertexShaderSource e
+// fragmentShader source no ini�io deste arquivo
+// A fun��o retorna o identificador do programa de shader
 int setupShader()
 {
 	// Vertex shader
@@ -198,8 +199,7 @@ int setupShader()
 	if (!success)
 	{
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-				  << infoLog << std::endl;
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 	// Fragment shader
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -240,44 +240,70 @@ int setupGeometry()
 	// sequencial, j� visando mandar para o VBO (Vertex Buffer Objects)
 	// Cada atributo do v�rtice (coordenada, cores, coordenadas de textura, normal, etc)
 	// Pode ser arazenado em um VBO �nico ou em VBOs separados
-	GLfloat vertices[] = {
-		-0.5, -0.5, 0.0,
-		 0.5, -0.5, 0.0,
-		 0.0, 0.5, 0.0,
-		 //outro triangulo vai aqui
-	};
+	
+		// static star shape
+	// GLfloat vetices[] = {
+	// 	0.0, 1.0, 0.0, // A 0
+	// 	-0.25, 0.5, 0.0, //c 1
+	// 	0.25, 0.5, 0.0, //d 2
+	// 	-1.0, 0.5, 0.0, //E 3
+	// 	1.0, 0.5, 0.0, //B 4
+	// 	-0.6, 0.0, 0.0, //b 5
+	// 	0.6, 0.0, 0.0, //e 6
+	// 	-0.8, -1.0, 0.0, // D 7
+	// 	0.8, -1.0, 0.0, // C 8
+	// 	0.0, -0.5, 0.0, // a 9
+	// };
+
+	
+
+	// unsigned int indices[] = {
+	// 	0,2,4,6,8,9,7,5,3,1
+	// 	// 0, 1, 2, // first triangle
+	// 	// 1, 3, 5,	 // second triangle
+	// 	// 2,4,6,
+	// 	// 5,7,9,
+	// 	// 9,8,6
+	// };
+
+	int segments = 7;
+	GLfloat* vertices = draw_circle(0.0f, 0.0f, 0.0f, 0.5f, segments);
+	for(int i =0; i < 21; i++){
+		std::cout << "vertices " << i << " value: " << vertices[i] << std::endl;
+	}
 
 	GLuint VBO, VAO;
+	int arraySize = (segments + 2) * 3 * sizeof(GLfloat);
+	std:: cout << arraySize << std::endl; 
 
-	//Gera��o do identificador do VBO
+	//Geração do identificador do VBO
 	glGenBuffers(1, &VBO);
-	//Faz a conex�o (vincula) do buffer como um buffer de array
+	//Faz a conexão (vincula) do buffer como um buffer de array
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//Envia os dados do array de floats para o buffer da OpenGl
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, arraySize, vertices, GL_STATIC_DRAW);
 
-	//Gera��o do identificador do VAO (Vertex Array Object)
+	//Geração do identificador do VAO (Vertex Array Object)
 	glGenVertexArrays(1, &VAO);
-	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de v�rtices
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
 	// e os ponteiros para os atributos 
 	glBindVertexArray(VAO);
 	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
-	// Localiza��o no shader * (a localiza��o dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	// Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
 	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
 	// Tipo do dado
-	// Se est� normalizado (entre zero e um)
+	// Se está normalizado (entre zero e um)
 	// Tamanho em bytes 
 	// Deslocamento a partir do byte zero 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	// Observe que isso � permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de v�rtice 
-	// atualmente vinculado - para que depois possamos desvincular com seguran�a
+	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
+	// atualmente vinculado - para que depois possamos desvincular com segurança
 	glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
-	// Desvincula o VAO (� uma boa pr�tica desvincular qualquer buffer ou array para evitar bugs medonhos)
+	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
 	glBindVertexArray(0); 
-
+	//free(vertices);
 	return VAO;
 }
-
